d6646ef62234bf4cd0eedc025062b2cbf746dbc0
ShihMengLi <shihsml@gmail.com>
2022-02-14 05:26:23 +0800

    aa

--- Examples/Monocular/mono_tum.cc
+++ Examples/Monocular/mono_tum.cc
@@ -139,6 +139,7 @@ int main(int argc, char **argv)
     cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
     cout << "mean tracking time: " << totaltime/nImages << endl;
 
+
     // Save camera trajectory
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 
